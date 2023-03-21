052106340ca01bac90aac6e3d9bbb991456e6d1e
daniel4o <danielddg.ss@gmail.com>
2023-03-21 18:47:15 +0200

    Started Hibernate & JPA section. Created a Java object with JPA.

---
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.cruddemo;
+
+import com.example.cruddemo.dao.StudentDAO;
+import com.example.cruddemo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Paul","Doe","pual@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+	}
+
+}
