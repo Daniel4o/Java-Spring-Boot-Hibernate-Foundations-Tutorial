377ab5795d1eb3ecf39806f3d291b332759f4ace
daniel4o <danielddg.ss@gmail.com>
2023-03-25 17:22:41 +0200

    Added Srping Data JPA. Finished Section 4 for REST CRUD with DB.

--- 04-spirng-boot-rest-crud/rest-crud-employees/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ 04-spirng-boot-rest-crud/rest-crud-employees/src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -12,34 +12,49 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeService employeeService;
+            private EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService) {
-        employeeService = theEmployeeService;
-    }
+            @Autowired
+            public EmployeeRestController(EmployeeService theEmployeeService) {
+                employeeService = theEmployeeService;
+            }
 
-    @GetMapping("/employees")
-    public List<Employee> findAll() {
-        return employeeService.findAll();
-    }
+            @GetMapping("/employees")
+            public List<Employee> findAll() {
+                return employeeService.findAll();
+            }
 
-    @GetMapping("/employees/{id}")
-    public Employee findById(int id) {
-        Employee theEmployee = employeeService.findById(id);
+            @GetMapping("/employees/{id}")
+            public Employee findById(int id) {
+                Employee theEmployee = employeeService.findById(id);
 
-        if(theEmployee == null) {
-            throw new EmployeeNotFoundException("Student id not found: " + id);
-        }
-        return theEmployee;
-    }
+                if(theEmployee == null) {
+                    throw new EmployeeNotFoundException("Student id not found: " + id);
+                }
+                return theEmployee;
+            }
 
-    @PostMapping("/employees")
-        public Employee addEmployee(@RequestBody Employee theEmployee) {
-            theEmployee.setId(0);
-            return employeeService.save(theEmployee);
-        }
+            @PostMapping("/employees")
+                public Employee addEmployee(@RequestBody Employee theEmployee) {
+                    theEmployee.setId(0);
+                    return employeeService.save(theEmployee);
+                }
 
+            @PutMapping("/employees")
+            public Employee updateEmployee(@RequestBody Employee theEmployee) {
+                return employeeService.save(theEmployee);
+            }
 
+            @DeleteMapping("/employees/{id}")
+            public String deleteEmployee(@PathVariable int id) {
+                Employee tempEmployee = employeeService.findById(id);
 
+                if(tempEmployee == null) {
+                    throw new EmployeeNotFoundException("Employee id not found - " + id);
+                }
+
+                employeeService.deleteById(id);
+
+                return "Deleted employee with id: " + id;
+            }
 }
