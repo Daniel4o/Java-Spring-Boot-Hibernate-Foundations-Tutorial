7abd82fe7cfccd2438c3059b255124a802d4de1a
daniel4o <danielddg.ss@gmail.com>
2023-03-28 18:34:37 +0300

    Added Spring Boot Security app. Started Spring Boot MVC Crud. Added custom HTML for tables, button for new employee and form for adding employee.

---
+++ 06-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/example/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.example.springboot.thymeleafdemo.controller;
+
+import com.example.springboot.thymeleafdemo.entity.Employee;
+import com.example.springboot.thymeleafdemo.service.EmployeeService;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	// add mapping for "/list"
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+
+		List<Employee> theEmployees = employeeService.findAll();
+
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/add")
+	public String addEmployee(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee",employee);
+		
+		return "employees/employee-form";
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ 06-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+	
+}
