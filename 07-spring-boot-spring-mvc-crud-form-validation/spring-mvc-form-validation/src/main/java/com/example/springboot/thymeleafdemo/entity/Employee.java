6572cb99dfe0fe82f68221dbaa162604bc2226bf
daniel4o <danielddg.ss@gmail.com>
2023-04-01 17:59:09 +0300

    Added custom validation using custom annotation. Added new attribute for Employee class.

--- 07-spring-boot-spring-mvc-crud-form-validation/spring-mvc-form-validation/src/main/java/com/example/springboot/thymeleafdemo/entity/Employee.java
+++ 07-spring-boot-spring-mvc-crud-form-validation/spring-mvc-form-validation/src/main/java/com/example/springboot/thymeleafdemo/entity/Employee.java
@@ -1,60 +1,72 @@
 package com.example.springboot.thymeleafdemo.entity;
 
+import com.example.springboot.thymeleafdemo.validation.EmployeeCode;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
-	
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String firstName;
-	
-	@NotNull(message="is required")
-	@Size(min=2, message="is required")
-	@Column(name="last_name")
+
+	@NotNull(message = "The field is required!")
+	@Size(min = 3, message = "The length is minimum 3 characters!")
+	@Column(name = "last_name")
 	private String lastName;
-	
-	@NotNull(message="is required")
-	@Size(min=2, message="is required")
-	@Column(name="email")
+
+	@Email(message = "Enter a valid email!")
+	@NotNull(message = "The field is required!!")
+	@Column(name = "email")
 	private String email;
 
-	@Min(value=0, message="must be greater than or equal to 0!")
-	@Max(value=10, message="must be greater than or equal to 10!")
-	@Column(name="priority")
-	private int priority;
-	
-	public Employee() {}
-	
-	public Employee(int id, String firstName, String lastName, String email, int priority) {
+	@NotNull(message = "The field is required!!")
+	@Min(value = 0, message = "Must be greater than or equal to 0!")
+	@Max(value = 10, message = "Must be lower than or equal to 10!")
+	@Column(name = "priority")
+	private Integer priority;
+
+	@Pattern(regexp="^[a-zA-z0-9]{10}", message="Code must be 10 characters!")
+	@EmployeeCode
+	private String employeeCode;
+
+	public Employee() {
+	}
+
+	public Employee(int id, String firstName, String lastName, String email, Integer priority, String employeeCode) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.priority = priority;
+		this.employeeCode = employeeCode;
 	}
 
-	public Employee(String firstName, String lastName, String email, int priority) {
+	public Employee(String firstName, String lastName, String email, Integer priority, String employeeCode) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.priority = priority;
+		this.employeeCode = employeeCode;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -87,28 +99,26 @@ public class Employee {
 		this.email = email;
 	}
 
-	public int getPriority() {
+	public Integer getPriority() {
 		return priority;
 	}
 
-	public void setPriority(int priority) {
+	public void setPriority(Integer priority) {
 		this.priority = priority;
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", priority=" + priority + "]";
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", priority=" + priority + "]";
 	}
-		
+
+	public String getEmployeeCode() {
+		return employeeCode;
+	}
+
+	public void setEmployeeCode(String employeeCode) {
+		this.employeeCode = employeeCode;
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
