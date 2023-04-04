32980139f731339308605986491add6da2ec2f8b
daniel4o <danielddg.ss@gmail.com>
2023-04-04 17:13:32 +0300

    Added Many-To-Many relationship project for students and courses.

---
+++ 08-hibernate-advanced-mappings/05-hibernate-many-to-many/src/main/java/com/example/hibernate/DeletePacmanCourseDemo.java
@@ -0,0 +1,63 @@
+package com.example.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.example.hibernate.entity.Course;
+import com.example.hibernate.entity.Instructor;
+import com.example.hibernate.entity.InstructorDetail;
+import com.example.hibernate.entity.Review;
+import com.example.hibernate.entity.Student;
+
+public class DeletePacmanCourseDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+
+			// get the pacman course from db
+			int courseId = 10;
+			Course tempCourse = session.get(Course.class, courseId);
+			
+			// delete the course
+			System.out.println("Deleting course: " + tempCourse);
+			
+			session.remove(tempCourse);
+						
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
